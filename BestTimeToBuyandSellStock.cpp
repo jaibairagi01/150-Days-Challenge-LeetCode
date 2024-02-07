bd6771f32a4199ea779eb3678824098b342afefd
Jai Bairagi <110719651+jaibairagi01@users.noreply.github.com>
2024-02-07 09:31:18 +0530

    Create BestTimeToBuyandSellStock.cpp

---
+++ BestTimeToBuyandSellStock.cpp
@@ -0,0 +1,15 @@
+#Day 7
+
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int mini = prices[0];
+        int profit = 0;
+        for(int i=0; i < prices.size();i++){
+            int diff = prices[i] - mini;
+            profit = max(profit, diff);
+            mini = min(mini, prices[i]);
+        }
+        return profit;
+    }
+};
