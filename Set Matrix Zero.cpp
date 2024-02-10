32398474bb6cd5575d40cecb3142d9d7ca36b421
Jai Bairagi <110719651+jaibairagi01@users.noreply.github.com>
2024-02-10 08:50:54 +0530

    Create Set Matrix Zero.cpp

---
+++ Set Matrix Zero.cpp
@@ -0,0 +1,26 @@
+#Day 10
+
+class Solution {
+public:
+    void setZeroes(vector<vector<int>>& matrix, int n, int m) {
+        int row[n] = {0};
+        int col[m] = {0};
+
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < m; j++){
+                if(matrix[i][j] == 0){
+                    row[i] = 1;
+                    col[j] = 1;
+                }
+            }
+        }
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < m; j++){
+                if(row[i] || col[j]){
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+        return matrix;
+    }
+};
