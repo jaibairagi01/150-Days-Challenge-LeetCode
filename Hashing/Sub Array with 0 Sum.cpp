2c01a65f95b56b8c6086f8b12cf3c97acb55fbda
Jai Bairagi <110719651+jaibairagi01@users.noreply.github.com>
2024-03-02 10:24:51 +0530

    Create Sub Array with 0 Sum.cpp

---
+++ Hashing/Sub Array with 0 Sum.cpp
@@ -0,0 +1,18 @@
+bool subArrayExists(int arr[], int n)
+    {
+        unordered_set<int> s;
+        
+        int pre_sum = 0;
+        for(int i=0; i<n; i++){
+            pre_sum += arr[i];
+            
+            if(pre_sum == 0)
+               return true;
+            if(s.find(pre_sum) != s.end())
+               return true;
+               
+            s.insert(pre_sum);
+        }
+        
+        return false;
+    }
